82620b068d338095fed26467efb0d828c9cb980c
Erhan Yaşar <mail@erhanyasar.com.tr>
2022-09-29 10:15:59 +0300

    deletes duplicates

--- js/portfolio/web_apps/hr_fileupload/in_browser/tests/pdf-to-text/main.js
+++
@@ -1,8 +0,0 @@
-    var pdfUtil = require('pdf-to-text');
-    var pdf_path = "sample.pdf";
-
-    pdfUtil.pdfToText(upload.path, function(err, data) {
-      if (err) throw(err);
-      console.log(data);   
-    });
-
